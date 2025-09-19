8129602cdbf62d89caa99f55ced489aa76066928
GRG1111 <gregvas385@gmail.com>
2025-09-19 16:38:56 +0700

    картинка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -26,4 +26,17 @@ def author():
                 <p>Факультет: """ + faculty + """</p>
                 <a href="/web">web</a>
            </body>
-        </html>"""   
+        </html>"""
+
+@app.route('/image')
+def image():
+    path = url_for("static", filename="oak.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + path + '''">
+    </body>
+</html>
+'''
