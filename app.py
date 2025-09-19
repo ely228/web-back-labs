4e21260ded90295a246b300ad2a881e0f3b338f5
GRG1111 <gregvas385@gmail.com>
2025-09-19 17:56:47 +0700

    ошибка 500

--- app.py
+++ app.py
@@ -237,3 +237,18 @@ def teapot():
 </body>
 </html>''', 418
 
+@app.route('/cause_500')
+def cause_500():
+    raise RuntimeError("Ошибка для проверки 500")
+
+@app.errorhandler(500)
+def handle_500(err):
+    return '''<!doctype html>
+<html>
+<head><meta charset="utf-8"><title>Ошибка 500</title></head>
+<body>
+  <h1>Внутренняя ошибка сервера (500)</h1>
+  <p>Произошла ошибка. Попробуйте позже.</p>
+  <a href="/">На главную</a>
+</body>
+</html>''', 500
