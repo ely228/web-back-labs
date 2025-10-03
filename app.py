f2a467deb676664af835424e3ae74cfcf54528b5
GRG1111 <gregvas385@gmail.com>
2025-10-03 19:27:19 +0700

    Имя студента

--- app.py
+++ app.py
@@ -341,4 +341,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Зубрицкий Илья'
+    return render_template('example.html', name=name)
