f7b5852f44980ee5987805d4de490565d4e47ec6
GRG1111 <gregvas385@gmail.com>
2025-10-31 01:17:20 +0700

    список пользователей

--- lab4.py
+++ lab4.py
@@ -118,8 +118,17 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=False)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверные логин или пароль'
-    return render_template('lab4/login.html', error=error)
+    return render_template('lab4/login.html', error=error, authorized=False)
+
+
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'dan', 'password': '111'},
+    {'login': 'abc', 'password': '444'}
+]
