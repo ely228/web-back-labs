ad7e1dd337bb99b402df8e328e51aaa5ad737b3d
GRG1111 <gregvas385@gmail.com>
2025-11-19 00:24:07 +0700

    Подключение бд SQlite

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ from lab4 import lab4
 from lab5 import lab5
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретный_ключ123')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
-app.secret_key = 'секретный_ключ123'
 
 
 @app.errorhandler(404)
