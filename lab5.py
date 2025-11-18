153ad6a6559c1f4a8bb0ff88515f1699365b0ad7
GRG1111 <gregvas385@gmail.com>
2025-11-18 22:36:25 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -9,14 +9,6 @@ lab5 = Blueprint('lab5', __name__)
 def index():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
-@lab5.route('/lab5/list')
-def article_list():
-    return render_template('lab5/list.html')
-
-@lab5.route('/lab5/create')
-def create_article():
-    return render_template('lab5/create.html')
-
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -76,3 +68,44 @@ def db_close(conn, cur):
     conn.commit()
     cur.close()
     conn.close()
+
+
+
+@lab5.route('/lab5/create', methods=['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s", (login, )) 
+    user_id = cur.fetchone()["id"]
+    
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text)\
+                VALUES ({user_id}, '{title}', '{article_text}');")
+
+    db_close(conn, cur)
+    return redirect('/lab5')
+
+
+@lab5.route('/lab5/list')
+def list(): 
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';") 
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles=articles)
